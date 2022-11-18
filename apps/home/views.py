51772a2e7762b3e8bf1dec4ab05813b39371042b
Teampandavas <118498247+Teampandavas@users.noreply.github.com>
2022-11-18 04:36:37 +0000

    Final

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,6 +11,10 @@ from django.template import loader
 from django.urls import reverse
 import json
 from apps.home.models import UserDetails
+from .post_forms import CreateNewPost
+import os
+from django.core.files.storage import FileSystemStorage
+
 # from django.forms.models import model_to_dict
 
 
@@ -64,3 +68,40 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+@login_required(login_url="/login/")
+def Create_NewPost(request):
+    # pass
+    # print(os.cu)
+    context = {'segment': 'index'}
+
+    form = CreateNewPost(request.POST or None)
+    # print("ding", form)
+    if request.method == "POST":
+        print(request.user)
+        # print(form.is_valid(),)
+
+        if form.is_valid():
+            try:
+                Title = form.cleaned_data.get("Title")
+                Description = form.cleaned_data.get("Desc")
+                myfile = request.FILES['vd']
+                fs = FileSystemStorage()
+                filename = fs.save(myfile.name, myfile)
+                uploaded_file_url = fs.url(filename)
+                print(uploaded_file_url)
+
+                print("djang",  Title, Description,uploaded_file_url)
+
+            except Exception as e:
+                pass
+
+            # with open(f"video/file/{Title}.mp4", 'wb+') as destination:
+            #     for chunk in video.chunks():
+            #         destination.write(chunk)
+
+    # html_template = loader.get_template('home/login.html')
+
+    return render(request, "home/NewPost.html", {"form": form, "msg": "msg"})
+    # return HttpResponse(html_template.render(context, request))
